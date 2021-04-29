e50e724adea5d80a6d843155743dffc6ebbc24ce
Arun Singh <arunsingh28aug.as@gmail.com>
2021-04-29 10:44:41 +0530

    new module

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const os = require('os')
 const http = require('http')
 const port = process.env.PORT || 5000
 
-http.createServer((req,res)=>{
+exports.server = http.createServer((req,res)=>{
     res.writeHead(200,{'conent-Type':'text/html'})
     res.end(`Hello ${os.userInfo().username}`,'utf-8')
 }).listen(port,console.log(`server runing on port : ${port}`))
+
