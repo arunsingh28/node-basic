ae686838a980060abc4089402aca82b2392c11d2
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-01 22:51:37 +0530

    http server error

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const os = require('os')
 const http = require('http')
+const net = require('net')
 const crp = require('./crypto')
-const {url} = require('url')
+const { url } = require('url')
 
 // const port = process.env.PORT || 5000
 
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'conent-Type':'text/html'})
-    res.end(`Hello ${os.userInfo().username}`,'utf-8')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'conent-Type': 'text/html' })
+    res.end(`Hello ${os.userInfo().username}`, 'utf-8')
 })
 
-server.on('connect',(req,clientSocket,head)=>{
-    const {port,hostname}= new URL(`http://${req.url}`)
-    const serverSocket = net.connect(port || 80, hostname,()=>{
+server.on('connect', (req, clientSocket, head) => {
+    const { port, hostname } = new URL(`http://${req.url}`)
+    const serverSocket = net.connect(port || 80, hostname, () => {
         clientSocket.write('HTTP connection Established.')
         serverSocket.write(head)
         serverSocket.pipe(clientSocket)
@@ -21,7 +22,10 @@ server.on('connect',(req,clientSocket,head)=>{
     })
 })
 
-server.listen(port,console.log(`server runing on port : ${port}`))
+server.listen(1337,()=>{
+    const req = http.request({port:1337,host:'127.0.0.1',method:'connect',path:'path:80'})
+    req.end()
+})
 module.exports = server
 
 
