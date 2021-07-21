22256cfa64ade18db66b07f50597a4b872f70dea
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-21 19:32:53 +0530

    create new file

--- file.js
+++ file.js
@@ -2,5 +2,20 @@ const fs = require('fs')
 
 fs.readFile('ct.csv','utf-8',(err,data)=>{
 	// file content store in data varibale
-	console.log(data)
+
+
+	// show file 
+	// console.log(data)
+
+
+
+	// create new file with data of that file
+
+
+	fs.writeFile('newCT.txt',data,(err)=>{
+		// if err occour
+		if(err) throw err
+
+		console.log('File Created Successfully.')
+	})
 })
