09359d78bfd0513eb025228200654a0561d819e6
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-21 22:27:09 +0530

    fs module

--- file.js
+++ file.js
@@ -1,21 +1,80 @@
 const fs = require('fs')
-
-fs.readFile('ct.csv','utf-8',(err,data)=>{
-	// file content store in data varibale
+const csv = require('csv-parser')
 
 
-	// show file 
-	// console.log(data)
+const result = []
+
+
+// fs.createReadStream('ct.csv')
+// 	.pipe(csv(['City','State']))
+// 	.on('data', (data) => result.push(data))
+// 	.on('end',()=>{
+// 		result.map(i => console.log(i.state))
+// 	})
+
+
+// fs.createReadStream('data.csv')
+// 	.pipe(csv())
+// 	.on('headers',(header)=>{
+// 		console.log(header[0])
+// 	})
+
+
+// *********** Create File ************ 
+
+// fs.createWriteStream('./output.js').write(
+// 	`
+// 	fs.createReadStream('ct.csv')
+// 		.pipe(csv(['City','State']))
+// 		.on('data', (data) => result.push(data))
+// 		.on('end',()=>{
+// 			result.map(i => console.log(i.state))
+// 		})
+
+
+// 	fs.createReadStream('data.csv')
+// 		.pipe(csv())
+// 		.on('headers',(header)=>{
+// 			console.log(header[0])
+// 		})
+// 	`
+// )
+
+// ************append date to exiting file ***************
+
+// let data = `\n const fs = require('fs')
+// const csv = require('csv-parser')
+
+
+// const result = []
+// `
+
+// fs.appendFileSync('output.ts', data)
+
+// ************rename exiting file ***************
+
+// fs.rename('./output.js', './output.ts', function (err) {
+// 	if (err) console.log('ERROR: ' + err);
+// 	console.log('successfull')
+// });
 
 
 
-	// create new file with data of that file
+// ************creating folder ***************
+
+// fs.mkdir('./NEW_folder',(err)=>{
+// 	if(err) throw err
+// 	console.log('successfull')
+// })
+// fs.mkdirSync('./newFolder')
 
 
-	fs.writeFile('newCT.txt',data,(err)=>{
-		// if err occour
-		if(err) throw err
 
-		console.log('File Created Successfully.')
-	})
-})
+// ************rename folder ***************
+
+// fs.renameSync('./newFolder','./Assets')
+
+
+// ************opem folder ***************
+
+fs.opendirSync('./Assets')
